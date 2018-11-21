21db1537c3bdb782a715c61a019c0c8f39da1e2f
hatamiarash7 <hatamiarash7@gmail.com>
2018-11-21 12:36:08 +0330

    make android project

---
+++ DCMotorController/app/src/main/java/ir/hatamiarash/dcmotorcontroller/Activity_main.java
@@ -0,0 +1,13 @@
+package ir.hatamiarash.dcmotorcontroller;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity_main extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
